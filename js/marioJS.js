d250e0bc8b8128720babd309a96db7370689af8a
Mysa33 <tpdrn.france@gmail.com>
2016-09-11 16:18:02 +0200

    corrections bug
    bug

--- js/marioJS.js
+++ js/marioJS.js
@@ -3,7 +3,7 @@ Mario JS
 Author : Sami Tlili
 */
 /*Mario go left*/
-function marioLeft(){ $('#mariO').stop().animate({left:'-=30'}); }
+function marioLeft(){ $('#mariO').stop().animate({left:'-=30'});}
 /*Mario go right*/  
 function marioRight(){ $('#mariO').stop().animate({left:'+=30'});}
 /*Mario jump*/  
@@ -12,15 +12,15 @@ function marioJump(){
     var jumpSound = "<audio id='jumpMarioAudio' autoplay><source src='sounds/saut.wav' type='audio/mpeg'></audio> ";//Jump sound
     $('#jumpMario').append(jumpSound);
     //Remove Jump Audio
-    function delMarioJumpSound(){ $('#jumpMarioAudio').remove(); }
+    function delMarioJumpSound(){ $('#jumpMarioAudio').remove();}
     setTimeout(delMarioJumpSound, 550);
 }
 /*Mario go down*/ 
 function marioDawn(){$('#mariO').animate({top:'+=30'});}
 /*Mario flip left*/ 
-function marioFlipFunc(){ $('#mariO').addClass("marioFlip"); }
+function marioFlipFunc(){ $('#mariO').addClass("marioFlip");}
 /*Mario flip right*/ 
-function marioFoward(){ $('#mariO').removeClass("marioFlip"); }
+function marioFoward(){ $('#mariO').removeClass("marioFlip");}
 /*Level background foward*/
 function backgroundTransitionRight(){
     $('.bgClass').stop().animate({backgroundPositionX:'-=50'});
@@ -36,15 +36,19 @@ function getMarioPosition(){
     var p = $( "#mariO" );
     var position = p.position();
     var positionLeft = position.left; 
-    var positionRight = position.right; 
+    //Max Bouttom 
     var positionTop = position.top;
     //ConsoleLog Dev
-    console.log("Mario positionLeft",positionLeft);
-    console.log("Mario positionTop",positionTop);
-    //Max Bouttom
+    console.log("Mario positionLeft", positionLeft);
+    console.log("Mario positionTop", positionTop);
     if(positionTop>540){p.css( "top", "540px" );}
     /*Mario left border*/
     if(positionLeft < 2){p.css( "left", "15px" );}
+    /*Mario right border*/
+    if(positionLeft > 850 ){
+        alert("right bounce");
+        p.css( "left", "- 15px" );
+    }
 }
 //Get Level postion/ Responsive
 function getLevelPosition(){
@@ -54,26 +58,32 @@ function getLevelPosition(){
     var positionRight = position.right; 
     var positionTop = position.top;
     //ConsoleLog Dev
-    console.log("BG positionLeft = ",positionLeft);
+    console.log("BG positionLeft = ", positionLeft);
 }
 //Get coin postion on load
 function getCoinPosition(){
     var p = $( ".coinOne" );
     var position = p.position();
-    var positionLeft = position.left; 
+    var positionLeftCoin = position.left; 
     var positionRight = position.right; 
     var positionTop = position.top;
     //ConsoleLog Dev
-    console.log("Coin positionLeft = ",positionLeft);
+    console.log("Coin positionLeft = ",positionLeftCoin);
     //CoinOne
-    if(positionLeft<330){
+    if(positionLeftCoin < 330){
         p.css( "display", "none" );
         var coinSound = "<audio id='coinMarioAudio' autoplay><source src='sounds/piece.wav' type='audio/mpeg'></audio> ";//Coin sound
         $('#coinMario').append(coinSound);
+        event.stopPropagation();
+    }
+    //Delete coin sound
+    if (positionLeftCoin == 0){
         //Remove Jump Audio
-        function delMarioCoinSound(){ $('#coinMarioAudio').remove(); }
-            setTimeout(delMarioCoinSound, 550);
-        }
+        function delMarioJumpSound(){ $('#coinMarioAudio').remove();}
+        setTimeout(delMarioJumpSound, 550);
+    $("#coinMario").remove(); 
+    }
+    
 }
 // Get Elements Positions
 $(document).ready(function() { getLevelPosition(); getMarioPosition(); getCoinPosition(); });
